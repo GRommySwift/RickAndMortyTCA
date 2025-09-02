da7aae35da3d42453f0784aed7f22fd84d9e8ed2
Roman <romaholovai@gmail.com>
2025-09-02 17:36:28 +0200

    initial commit
    created new project

---
+++ RickAndMortyTCA/RickAndMortyTCATests/RickAndMortyTCATests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTCATests.swift
+//  RickAndMortyTCATests
+//
+//  Created by Roman on 02/09/2025.
+//
+
+import Testing
+@testable import RickAndMortyTCA
+
+struct RickAndMortyTCATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
